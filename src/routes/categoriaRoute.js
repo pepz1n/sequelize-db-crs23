7e7e417cc496e66489d6b5baecf61f7d87767c8b
Bernardo <bebezanetti@gmail.com>
2023-06-14 14:07:06 -0300

    feat: primeiro Crud

--- src/routes/categoriaRoute.js
+++ src/routes/categoriaRoute.js
@@ -1,9 +1,9 @@
 import controller from '../controllers/categoriaController'
 
 export default (app) => {
-	app.post('/categorias/deletar', controller.destroy)
+	app.delete('/categorias/:id', controller.destroy)
 	app.get('/categorias', controller.get)
 	app.get('/categorias/:id', controller.get)
 	app.post('/categorias', controller.persist)
-	app.post('/categorias/:id', controller.persist)
+	app.patch('/categorias/:id', controller.persist)
 }
